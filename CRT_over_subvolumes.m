7a91bd8b8b8b6f9acca2e9396a64a7b1a4ecc518
aleejessop <58460392+aleejessop@users.noreply.github.com>
2023-01-20 11:09:26 +0800

    Update CRT_over_subvolumes.m

--- CRT_over_subvolumes.m
+++ CRT_over_subvolumes.m
@@ -1,25 +1,14 @@
 clear
-filename = ('C:\Users\20220428\OneDrive - Murdoch University\Documents\MATLAB\Sea_Urchin\data\greyscale\1_69_inner_interambulacral_plate_transformed.tif');
+filename = ('C:\Users\20220428\OneDrive - Murdoch University\Documents\MATLAB\Sea_Urchin\data\greyscale\1_69_inner_interambulacral_plate_transformed_cropped_for_CRT.tiff');
 greyscale = tiffreadVolume(filename);
 binary = imbinarize(greyscale,0.51);
-binary = binary(201:1200,181:1180,301:1300);
+% binary = binary(201:1200,181:1180,301:1300);
+binary = binary(1:400,1:1200,1:1000);
 binary = 1-binary;
-imagesc(squeeze(binary(800,:,:))); axis equal
+imagesc(squeeze(binary(200,:,:))); axis equal
 size_subvolume = 50;
 [subvolume] = subvolume_extraction(binary, size_subvolume);
 volshow(squeeze(subvolume(1,1,2,:,:,:)))
-
-for a = 1:size(subvolume,1)
-    for b = 1:size(subvolume,2)
-        for c = 1:size(subvolume,3)
-            if mean(subvolume(a,b,c,:,:,:)) == 0
-                subvolume(a,b,c,:,:,:) = 1;
-            else
-                subvolume(a,b,c,:,:,:) = subvolume(a,b,c,:,:,:);
-            end
-        end
-    end
-end
 
 image0 = zeros(20,20,20,50,50,50);
 image1 = zeros(20,20,20,50,50,50);
@@ -35,6 +24,11 @@ for i = 1:size(subvolume,1)
     end
 end
 
-volshow(squeeze(image0(1,1,3,:,:,:)))
+CRT_data.pore_sizes_solid = image0;
+CRT_data.filename = filename;
+CRT_data.subvolume_size = 50;
+CRT_data_pore_sizes_solid_max_centre = image1;
+CRT_data.voxels = [1:400,1:1200,1:1000];
+CRT_data.pore_sizes_solid_max = image1;
 
-maxCoveringDistanceTransform(squeeze(subvolume(5,5,5,:,:,:)));
+save('CRT_cropped_data.mat','CRT_data','-v7.3')
