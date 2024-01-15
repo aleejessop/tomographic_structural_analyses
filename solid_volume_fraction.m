3e2d8377fa7c1b3e3714f288f15093449f392f56
aleejessop <aleejessop@gmail.com>
2024-01-15 10:55:50 +0800

    Update solid_volume_fraction.m

--- solid_volume_fraction.m
+++ solid_volume_fraction.m
@@ -4,7 +4,7 @@ function [volume_fraction] = solid_volume_fraction(subvolume)
 for i = 1:size(subvolume,3)
     for j = 1:size(subvolume,2)
         for k = 1:size(subvolume,1)
-            volume_fraction(k,j,i) = mean(subvolume(k,j,i,:,:,:),'all');
+            volume_fraction(k,j,i) = nanmean(subvolume(k,j,i,:,:,:),'all');
         end
     end
 end
